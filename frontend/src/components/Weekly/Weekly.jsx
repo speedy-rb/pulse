7fba0eedd4a1ee8c3d8bb419cf23c620741693bc
Speedy <speedy@roshibros.com>
2025-08-31 20:00:22 -0500

    Set pictures week of 08312025

--- frontend/src/components/Weekly/Weekly.jsx
+++ frontend/src/components/Weekly/Weekly.jsx
@@ -1,23 +1,40 @@
 import styles from "./Weekly.module.css";
-import imgSrc1 from '../../assets/stories/506348862_18071130554479484_4617004184185470533_n.jpg'
-import imgSrc2 from '../../assets/stories/511327035_18516178342065299_3976111128338068787_n.jpg'
-import imgSrc3 from '../../assets/stories/514313135_18277222510285707_6512502184883222966_n.jpg'
-import imgSrc4 from '../../assets/stories/515295740_18032639195678785_4193119965195457976_n.jpg'
-import imgSrc5 from '../../assets/stories/515461670_18514839649065299_4590827714690636270_n.jpg'
-import imgSrc6 from '../../assets/stories/515660226_18358021081154646_3267377219321656423_n.jpg'
-import imgSrc7 from '../../assets/stories/515757486_18410511601105805_3395155817585086560_n.jpg'
+import imgSrc1 from '../../assets/08312025/518235653_18511130893045941_5661599903389406608_n.jpg'
+import imgSrc2 from '../../assets/08312025/516818439_18349122532084215_6804311255893000883_n.jpg'
+import imgSrc3 from '../../assets/08312025/506348862_18071130554479484_4617004184185470533_n.jpg'
+import imgSrc4 from '../../assets/08312025/photo_7.jpg'
+import imgSrc5 from '../../assets/08312025/516344363_18168781681355582_802596286817281210_n.jpg'
+import imgSrc6 from '../../assets/08312025/505495169_18121168714473166_8698424718009572581_n.jpg'
+import imgSrc7 from '../../assets/08312025/517022446_18380085310123894_3739198221313845084_n.jpg'
 
 function Weekly() {
     const images = [imgSrc1, imgSrc2, imgSrc3, imgSrc4, imgSrc5, imgSrc6, imgSrc7]
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     return (
         <div>
             <div className={styles.header}>Header</div>
             <div className={styles.calendar}>
-                <div className={styles.calendarColumn}>time slots</div>
+                <div className={styles.calendarTimeSlots}>time slots</div>
+                {days.map((day, i) => (
+                    <div key={i}
+                        className={styles.calendarDayHeader}
+                        style={{gridColumn: i+2}}
+                    >
+                        {day}
+                    </div>
+                ))}
                 {images.map((src, i) => (
-                    <div key={i} className={styles.calendarColumn}>
+                    <div key={i}
+                        className={styles.calendarColumn}
+                        style={{gridColumn: i+2}}
+                    >
                         <img src={src}></img>
                     </div>
+                ))}
+                {Array.from({length: 7}).map((_, i) => (
+                    <div key={i}
+                        style={{gridColumn: i+2}}
+                    >content</div>
                 ))}
             </div>
             <div className={styles.addEventBtn}>Add Event</div>
