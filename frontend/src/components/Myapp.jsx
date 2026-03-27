62fb3615aa13d8ff1a164bdbeba0aa7811189ee6
john <speedy@roshibros.com>
2026-03-26 19:01:47 -0500

    Add column dividing lines, center cells, and align header columns with body columns

--- frontend/src/components/Myapp.jsx
+++ frontend/src/components/Myapp.jsx
@@ -4,16 +4,22 @@ import styles from './Myapp.module.css';
 const entityData = [
   {
     'label': 'num1',
-    'date': "2026-03-27",
+    'date': "2026-03-26",
     'img': 'coolpic',
     'id': 1
   },
   {
     'label': 'num2',
-    'date': "2026-03-26",
+    'date': "2026-03-27",
     'img': 'coolpic2',
     'id': 2
-  }
+  },
+  {
+    'label': 'num3',
+    'date': "2026-03-28",
+    'img': 'coolpic3',
+    'id': 3
+  },
 ]
 const fields =  Object.keys(entityData[0]);
 function Myapp() {
@@ -21,30 +27,37 @@ function Myapp() {
       <div className={styles.header}>
         <div className={styles.appHeader}>
           <div className={styles.appHeaderLeft}>
-            <div>hamburger</div>
+            <div>menu</div>
             <div>month</div>
           </div>
           <div className={styles.appHeaderRight}>
             <div>profile</div>
           </div>
         </div>
-        <div className={styles.entityLabelHeader}>
+        <div className={styles.entityRow}>
+          <div className={styles.cell}></div>
           {entityData.map(entity => (
-            <div key={entity.id}>{entity.label}</div>
+            <div className={styles.cell} key={entity.id}>{entity.date}</div>
           ))}
         </div>
       </div>
+
       <div className="body">
         {fields.map(field => (
           <div key={field} className={styles.entityRow}>
-            <div>{field}</div>
+            <div className={styles.cell}>{field}</div>
             {entityData.map(entity => (
-              <div key={`${entity['id']}.${field}`}>{entity[field]}</div>
+              <div key={`${entity['id']}.${field}`} className={styles.cell}>{entity[field]}</div>
             ))}
           </div>
         ))}
         <div className={styles.entityRow}>
-          add new field
+          <div className={styles.cell}>
+            add new field
+          </div>
+          {entityData.map(entity => (
+            <div className={styles.cell}></div>
+          ))}
         </div>
       </div>
     </>
