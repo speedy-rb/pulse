8252e5756a6052f4527050a4e1d6305a51755a15
john <speedy@roshibros.com>
2026-03-28 16:12:54 -0500

    Attempt to fix scrolling past sticky header on PWA

--- frontend/src/components/Myapp.jsx
+++ frontend/src/components/Myapp.jsx
@@ -31,67 +31,69 @@ function Myapp() {
   const entityData = createEntityData(activeDate);
   const fieldsToShow =  ['label','date','img','id'];
 
-  return <>
-    <div className={styles.header}>
-      <div className={styles.appHeader}>
-        <div className={styles.appHeaderLeft}>
-          <div className={styles.hamburger}>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <div className={styles.monthArrow} onClick={toggleCalendar}>
-            <div>{activeDate.format("MMMM")}</div>
-            <svg
-              className={`${styles.arrow} ${isCalendarExpanded ? styles.arrowExpanded : ""}`}
-              viewBox="0 0 10 10" width="10" height="10">
-              <polygon points="0,0 10,5 0,10" fill="black" />
-            </svg>
-          </div>
-        </div>
-        <div className={styles.appHeaderRight}>
-          <img src='pfp-64-64.png' className={styles.pfp} />
-        </div>
-      </div>
-      {isCalendarExpanded && (
-        <Calendar
-          activeDate={activeDate}
-          setActiveDate={setActiveDate}
-        />
-      )}
-      <div className={`${styles.entityLabelRow} ${
-          isCalendarExpanded ? styles.entityLabelRowExpanded : ""
-        }`}>
-        <div className={styles.cell}></div>
-        {entityData.map(entity => (
-          <div className={styles.cell} key={entity.id}>
-            <div className={styles.dayHeaderContainer}>
-              <div className={styles.dayHeaderWeekDay}>{entity.dateObj.format('ddd').toUpperCase()}</div>
-              <div className={styles.dayHeaderDay}>{entity.dateObj.date()}</div>
+  return (
+    <div className={styles.appShell}>
+      <div className={styles.header}>
+        <div className={styles.appHeader}>
+          <div className={styles.appHeaderLeft}>
+            <div className={styles.hamburger}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className={styles.monthArrow} onClick={toggleCalendar}>
+              <div>{activeDate.format("MMMM")}</div>
+              <svg
+                className={`${styles.arrow} ${isCalendarExpanded ? styles.arrowExpanded : ""}`}
+                viewBox="0 0 10 10" width="10" height="10">
+                <polygon points="0,0 10,5 0,10" fill="black" />
+              </svg>
             </div>
           </div>
-        ))}
-      </div>
-    </div>
-    <div className="body">
-      {fieldsToShow.map(field => (
-        <div key={field} className={styles.entityRow}>
-          <div className={styles.cell}>{field}</div>
+          <div className={styles.appHeaderRight}>
+            <img src='pfp-64-64.png' className={styles.pfp} />
+          </div>
+        </div>
+        {isCalendarExpanded && (
+          <Calendar
+            activeDate={activeDate}
+            setActiveDate={setActiveDate}
+          />
+        )}
+        <div className={`${styles.entityLabelRow} ${
+            isCalendarExpanded ? styles.entityLabelRowExpanded : ""
+          }`}>
+          <div className={styles.cell}></div>
           {entityData.map(entity => (
-            <div key={`${entity['id']}.${field}`} className={styles.cell}>{entity[field]}</div>
+            <div className={styles.cell} key={entity.id}>
+              <div className={styles.dayHeaderContainer}>
+                <div className={styles.dayHeaderWeekDay}>{entity.dateObj.format('ddd').toUpperCase()}</div>
+                <div className={styles.dayHeaderDay}>{entity.dateObj.date()}</div>
+              </div>
+            </div>
           ))}
         </div>
-      ))}
-      <div className={styles.entityRow}>
-        <div className={styles.cell}>
-          add new field
-        </div>
-        {entityData.map((entity, i) => (
-          <div className={styles.cell} key={i}></div>
+      </div>
+      <div className="body">
+        {fieldsToShow.map(field => (
+          <div key={field} className={styles.entityRow}>
+            <div className={styles.cell}>{field}</div>
+            {entityData.map(entity => (
+              <div key={`${entity['id']}.${field}`} className={styles.cell}>{entity[field]}</div>
+            ))}
+          </div>
         ))}
+        <div className={styles.entityRow}>
+          <div className={styles.cell}>
+            add new field
+          </div>
+          {entityData.map((entity, i) => (
+            <div className={styles.cell} key={i}></div>
+          ))}
+        </div>
       </div>
     </div>
-  </>
+  )
 }
 
 export default Myapp
