2478730470607257f2561438af6186e311f2d921
john <speedy@roshibros.com>
2026-04-04 16:51:53 -0500

    Fix incorrect pwa icon filename

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,12 +18,12 @@ registerType: 'autoUpdate',
         theme_color: "#bbf1ffff",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/pulse-icon-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/pwa-512x512.png",
+            src: "/pulse-icon-512x512.png",
             sizes: "512x512",
             type: "image/png"
           }
